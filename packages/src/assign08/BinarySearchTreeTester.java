03af70cd2affab0621a7a66eec3f98f526c97024
VivekVankayalapati <vivekvankayalapati@gmail.com>
2020-03-17 10:02:28 -0600

    fixed junit

--- packages/src/assign08/BinarySearchTreeTester.java
+++ packages/src/assign08/BinarySearchTreeTester.java
@@ -2,7 +2,7 @@ package assign08;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;  //I am having issues with junit
+import org.junit.jupiter.api.Test; 
 
 class BinarySearchTreeTester {
 
