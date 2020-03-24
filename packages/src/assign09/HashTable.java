e1692ebcea02836f7fc09dd7f82a9165e5f5e828
Brady Hartog <bjhartog@me.com>
2020-03-24 11:06:45 -0600

    Implemented methods in HashTable

--- packages/src/assign09/HashTable.java
+++ packages/src/assign09/HashTable.java
@@ -1,13 +1,26 @@
 package assign09;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class HashTable<K, V> implements Map<K, V>
 {
+	private ArrayList<LinkedList<MapEntry<K, V>>> table;
+	private int size;
+	// private int capacity?
+	
+	public HashTable()
+	{
+		table = new ArrayList<LinkedList<MapEntry<K, V>>>();
+		for(int i = 0; i < capacity; i++)
+		   table.add(new LinkedList<MapEntry<K, V>>());
+	}
+	
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		this.table = new ArrayList<LinkedList<MapEntry<K, V>>>();
+		this.size = 0;
 	}
 
 	@Override
@@ -23,27 +36,46 @@ public class HashTable<K, V> implements Map<K, V>
 	}
 
 	@Override
-	public List<MapEntry<K, V>> entries() {
-		// TODO Auto-generated method stub
+	public List<MapEntry<K, V>> entries()
+	{
+		return this.table;
+	}
+
+	@Override
+	public V get(K key)
+	{
 		return null;
 	}
 
 	@Override
-	public V get(K key) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
+	public boolean isEmpty()
+	{
+		if (size == 0)
+		{
+			return true;
+		}
+		
 		return false;
 	}
 
 	@Override
-	public V put(K key, V value) {
-		// TODO Auto-generated method stub
-		return null;
+	public V put(K key, V value)
+	{
+		if (containsKey(key) == false)
+		{
+			MapEntry<K, V> entry = new MapEntry<K, V>(key, value);
+			table.add(entry);
+			
+			size++;
+			
+			return value;
+		}
+		else
+		{
+			// Get MapEntry, modify value
+			
+			return null;
+		}
 	}
 
 	@Override
@@ -53,8 +85,8 @@ public class HashTable<K, V> implements Map<K, V>
 	}
 
 	@Override
-	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int size()
+	{
+		return this.size;
 	}
 }
